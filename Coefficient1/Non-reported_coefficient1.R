c6518a51d8f4baa5269b7adec3f62c876ed8e98d
laljeet <laljeet@vt.edu>
2021-03-15 09:26:25 -0400

    Coefficient1 Plots

--- Coefficient1/Non-reported_coefficient1.R
+++ Coefficient1/Non-reported_coefficient1.R
@@ -1,19 +1,26 @@
 WUDR_github<-"F:/My Drive/WUDR/WUDR_Github/WUDR_local"
 setwd(WUDR_github)
+# load libraries
+library(sqldf)
+library(tidyverse)
+library(dplyr)
+library(rgdal)
+library(compare)
 
 load(paste0(WUDR_github,"/dat_load/dat_D_filled.RData"))
 load(paste0(WUDR_github,"/dat_load/opeartions_list.RData"))
 
-
+sum(dat_D_filled[[1]][2], na.rm=TRUE)
 
 
 new_df2 <- lapply(opeartions_list, function(x){
   x$'Size bin' <- rownames(x)
   rownames(x) <- NULL
+  x <- x[,c(1,2)]
   return(x)
 })
 
-total <- Map(merge, dat_D_filled,new_df2, by = 'Size bin')
+total <- Map(merge, dat_D_filled,new_df2, by.x = 'Size bin', by.y = 'ops.bins')
 
 total <- lapply(total, function(x){
   x<- x[c(2,3,10,12,4,6,7,8,9,11,5,1),]
@@ -74,17 +81,21 @@ colnames <- c("Irr.Area.Under.TH" , "Irr.Area.Over.TH")
 TH_dat <- lapply(TH_dat, setNames, colnames)
 
 TH_dat <- lapply(TH_dat, function(x){
-  x$Pct.Non.repoted <- round (100*x$Irr.Area.Under.TH / x$Irr.Area.Over.TH, 2)
+  x$Pct.Non.repoted.of.total.irrarea <- round (100*x$Irr.Area.Under.TH / sum(x$Irr.Area.Over.TH,x$Irr.Area.Under.TH), 2)
+  x$Pct.Non.repoted.of.area.abv.TH <- round (100*x$Irr.Area.Under.TH / x$Irr.Area.Over.TH, 2)
   return(x)
 })
 
-Unreported <-  lapply(TH_dat, function(x) x%>% select(Pct.Non.repoted, Irr.Area.Under.TH))
+
+Unreported <-  lapply(TH_dat, function(x) x%>% select(Pct.Non.repoted.of.total.irrarea,Pct.Non.repoted.of.area.abv.TH, Irr.Area.Under.TH,Irr.Area.Over.TH))
 
 
 df <- data.frame(matrix(unlist(Unreported), nrow=length(Unreported), byrow=TRUE))
 df$county <- names(Unreported)
 
-colnames(df) <- c("Pct.Non.repoted", "Irr.Area.Under.TH", "County") 
-df <- df[,c(3,2,1)]
+colnames(df) <- c("Pct.under.TH.of.total.Irr.area", "Pct.under.TH.of.Irri.area.abv.TH","Irr.Area.Under.TH", "Irr.Area.above.TH", "County") 
+df$Total.Irri.Area <- apply(df[,c(3,4)],1,sum)
+df <- df[,c(5,6,4,3,2,1)]
 
+write.csv(df, paste0(WUDR_github,"/csv_files/Percentage Irri.data.Under TH.csv"))
 
--- USDA_census_data/D values substituted and rescaled.R
+++ USDA_census_data/D values substituted and rescaled.R
@@ -60,7 +60,7 @@ summary.irr$County.sum<- ifelse(is.na(summary.irr$County.sum), summary.irr$Irri.
 
 summary.irr$D_area <- summary.irr$County.sum - summary.irr$Irri.sum
 
-
+# write.csv(summary.irr ,paste0(WUDR_github,"/csv_files/2017census.irrigated_diff.csv"))
 
 #########################Select size bins with D values  in sorted list
 
@@ -102,6 +102,8 @@ for (i in 1: length(area_list)){
   area_list[[i]] <- as.data.frame(t(area_list[[i]]))
 }
 
+
+
 names_list <- names(area_list)
 sorted <- list()
 for (i in 1:94) {
@@ -122,22 +124,65 @@ size.names <- c( "below10", "between_10_50","between_50_70","between_70_100"
 bin.char <- read.csv(paste0(WUDR_github,"/csv_files/bin.char.csv"), header=TRUE)
 bin.char$size <- size.names
 
+
+###########################################################################
+#Load operations data ##################################################
+######################################################################
+
+Operations.data <- read.csv(paste0(WUDR_github,"/csv_files/binned_operations.csv"))
+Operations.data[is.na(Operations.data)] <- -99
+
+
+Colnames <- c("County", "below10", "between_10_50","between_50_70","between_70_100" 
+              ,"between_100_139","between_140_179","between_180_219","between_220_259","between_260_499"
+              ,"between_500_999","between_1000_1999", "Above_2000")
+
+colnames(Operations.data) <- Colnames
+
+rownames(Operations.data) <- Operations.data[,1]
+
+Operations.data <- Operations.data[,-1]
+
+
+opeartions_list <- setNames(split(Operations.data, seq(nrow(Operations.data))), rownames(Operations.data))
+
+for (i in 1: length(opeartions_list)){
+  opeartions_list[[i]] <- as.data.frame(t(opeartions_list[[i]]))
+}
+
+for (i in 1:94) {
+  x <- as.data.frame(opeartions_list[[i]]) 
+  
+  x$ops.bins <- rownames(x)
+  x <- x[,c(2,1)]
+  opeartions_list[[i]] <- x
+  rownames(opeartions_list[[i]]) <- NULL
+} 
+
+##########################################################################
+#########################################################################
 pct.irr <- list ()
 
 for (i in 1:94) {
   x <- as.data.frame(sorted[[i]]) 
-  x <-  merge(bin.char[,c(2,8)], x , by.x = "size", by.y = "size.bins")
-  x <- as.data.frame(x[,c(1:2)])
-  pct.irr[[i]] <- x
+  x <-  merge(bin.char[,c(2,5,8)], x , by.x = "size", by.y = "size.bins")
+  x <- as.data.frame(x[,c(1:2,3)])
+  
+  y <- as.data.frame(opeartions_list[[i]]) 
+  
+  z <- merge(x,y , by.x = "size", by.y = "ops.bins")
+  pct.irr[[i]] <- z
 }  # This list contains the percentage area irrigated in each size bin where D values are absent
 
+colnames <- c("Size", "Avg_Size", "Avg_Pct_Irri", "Ops")
+
+pct.irr <- lapply(pct.irr, setNames, colnames)
 
 area.irr <- list()
 for (i in 1:94) {
   
-  
   x <- as.data.frame(pct.irr[[i]])
-  x$area <- x$avg_perc_irr * summary.irr[i,2]
+  x$area <- x$Avg_Pct_Irri * x$Avg_Size * x$Ops
   
   area.irr[[i]] <- x
 }  # Irrigated area for D values
@@ -147,15 +192,15 @@ recaled_dat <- area.irr
 
 for (i in 1:94) {
   
-  recaled_dat[[i]][4] <- Map(function(x, y)  x * y/sum(x), area.irr[[i]][3], as.numeric(summary.irr[i,4]))
+  recaled_dat[[i]][6] <- Map(function(x, y)  x * y/sum(x), area.irr[[i]][5], as.numeric(summary.irr[i,4]))
   
 }
 
-sum(recaled_dat[[1]][4])
+sum(recaled_dat[[1]][6])
 
 # THis rescaled dat list replaces the D values after reclaing it to difference
 
-colnames <- c("Size bin", "Average Pct Irri", " Area Irrigated", " Rescaled Area")
+colnames <- c("Size bin", "Avg_Size", "Average Pct Irri", "Ops", "Area Irrigated", " Rescaled Area")
 
 recaled_dat <- lapply(recaled_dat, setNames, colnames)
 
@@ -172,11 +217,13 @@ for (i in 1:94) {
   y <- as.data.frame(area_list[[i]])
   y$size <- rownames(y)
   z <-  full_join(x, y , by = c("Size bin"= "size"))
-  z[,5] <- ifelse((z[,5] == -9999), z[,4], z[,5])
-  z <- as.data.frame(z[,c(1,5)])
+  z[,7] <- ifelse((z[,7] == -9999), z[,6], z[,7])
+  z <- as.data.frame(z[,c(1,7)])
   z[,2] <- round(z[,2], 0)
   dat_D_filled[[i]] <- z
 }
+
+sum(dat_D_filled[[1]][2], na.rm=TRUE)
 
 colnames <- c("Size bin",  "Area_irr_rescaled Area")
 
@@ -184,11 +231,8 @@ colnames <- c("Size bin",  "Area_irr_rescaled Area")
 names(dat_D_filled) <- names_list
 
 save(dat_D_filled, file=paste0(WUDR_github,"/dat_load/dat_D_filled.RData"))
-sum(dat_D_filled[[1]][2], na.rm = TRUE)
 
 
-######################################################################################
-##### Calculate Area below TH
 
 
 
