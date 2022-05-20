be18b5c721bfe1f2ab1ea03daec4e1078f1afa60
Burgholzer <robert.burgholzer@deq.virginia.gov>
2022-05-20 12:11:14 -0400

    iterate with ideas for reasonableness of coefficient

--- R/compare_methods/irr_vs_coeff-small.R
+++ R/compare_methods/irr_vs_coeff-small.R
@@ -19,13 +19,30 @@ cmp_data <- sqldf(
       a.Unreported_Coeff_Based,
       b.Unreported_Irrigation_based,
       b.Irrigation,
-      b.PPT
+      b.PPT,
+      Unreported_Irrigation_based / Facility_withdrawal_mg as yr_coeff
     from small_f_vwuds as a
     left outer join small_f_precip as b
     on (
       a.COUNTYFP = b.County_Code
       and a.Year = b.Year
 
+    )
+  "
+)
+
+cmp_data <- sqldf(
+  "
+    select a.*, b.med_coeff,
+      b.med_coeff * a.Facility_withdrawal_mg as med_coeff_mg
+    from cmp_data as a
+    left outer join (
+      select County_Name, median(yr_coeff) as med_coeff
+      from cmp_data
+      group by County_Name
+    ) as b
+    on (
+      a.County_name = b.County_Name
     )
   "
 )
@@ -43,11 +60,59 @@ irrlm <- lm(
 summary(irrlm)
 abline(irrlm, col="red")
 
+# now do the median coefficient
+plot(
+  med_coeff_mg ~ Unreported_Irrigation_based,
+  data=cmp_data,
+  ylim=c(0,3000),
+  xlim=c(0,3000)
+)
+irrlm <- lm(
+  med_coeff_mg ~ Unreported_Irrigation_based,
+  data=cmp_data
+)
+summary(irrlm)
+abline(irrlm, col="red")
+
+
+# cmp data that isreasonable, no > 1.0 coefficients
+cmp_data_reas <- sqldf("select * from cmp_data where Mean_Coeff < 1.0")
+plot(
+  Unreported_Coeff_Based ~ Unreported_Irrigation_based,
+  data=cmp_data_reas,
+  ylim=c(0,500),
+  xlim=c(0,500)
+)
+irrlm <- lm(
+  Unreported_Coeff_Based ~ Unreported_Irrigation_based,
+  data=cmp_data_reas
+)
+summary(irrlm)
+abline(irrlm, col="red")
+
+# cmp data that isreasonable, no > 1.0 coefficients
+cmp_data_reas <- sqldf("select * from cmp_data where med_coeff < 1.0")
+plot(
+  med_coeff_mg ~ Unreported_Irrigation_based,
+  data=cmp_data_reas,
+  ylim=c(0,500),
+  xlim=c(0,500)
+)
+irrlm <- lm(
+  med_coeff_mg ~ Unreported_Irrigation_based,
+  data=cmp_data_reas
+)
+summary(irrlm)
+abline(irrlm, col="red")
+
 # identify a couple outliers to ask about
 test_case <- sqldf(
   "
-  select * from cmp_data
+  select *,
+    Unreported_Irrigation_based / Facility_withdrawal_mg as yr_coeff
+  from cmp_data
   where County_Name = 'LOUDOUN'
+  and Year in (2002,2007,2012,2017)
   "
 )
 # If Loudoun has reported irrigation ave 23.2 mgy,
